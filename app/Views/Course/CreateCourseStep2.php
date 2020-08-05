044caba5618ac18690ff0237541f384a20128601
saen123456 <6006021611216@fitm.kmutnb.ac.th>
2020-08-05 18:29:03 +0700

    test

--- app/Views/Course/CreateCourseStep2.php
+++ app/Views/Course/CreateCourseStep2.php
@@ -264,7 +264,7 @@
                     $.ajax({
                         <?php $count = "<script>document.writeln(Unit_Count)</script>"; ?>
                         url: "<?php
-                                echo site_url('/CourseController/Upload_Unit?unit=" . $count . "');
+                                echo site_url('/CourseController/Upload_Unit?unit=' . $count . '');
                                 // echo site_url('/CourseController/Upload_Test');
                                 ?>",
                         type: "POST",
