eb0adac3c7a1558e2ca88a9dc15fc00d1792312a
saen123456 <6006021611216@fitm.kmutnb.ac.th>
2020-08-05 18:53:07 +0700

    test

--- app/Views/Course/CreateCourseStep2.php
+++ app/Views/Course/CreateCourseStep2.php
@@ -262,11 +262,12 @@
                     var xhr = new window.XMLHttpRequest();
 
                     $.ajax({
-                        <?php $count = "<script>document.writeln(Unit_Count)</script>"; ?>
-                        url: "<?php
-                                echo site_url('/CourseController/Upload_Unit?unit=' . $count . '');
-                                // echo site_url('/CourseController/Upload_Test');
-                                ?>",
+                        url: "https://workgress.online/CourseController/Upload_Unit?unit=" + Unit_Count,
+                        // url: "<?php
+                                    //         echo site_url('/CourseController/Upload_Unit?unit=' . $count . '');
+                                    //         // echo site_url('/CourseController/Upload_Test');
+                                    //         
+                                    ?>",
                         type: "POST",
                         data: new FormData($form[0]),
                         contentType: false,
